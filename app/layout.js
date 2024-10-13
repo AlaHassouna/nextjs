8b89c0907e4ac4303928c777a8eaf396ab9986b9
Hsouna <ala.hassouna.tn@gmail.com>
2024-10-13 22:05:31 +0100

    supprimer modification icon onglet

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,6 @@
-import Head from 'next/head';
-
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <Head>
-        <link rel="icon" href="/logo.png" />
-        <title>Mon site</title>
-      </Head>
         <body>
           {children}
         </body>
