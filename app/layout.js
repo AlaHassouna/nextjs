419e5273f082fcd24a721819645ec00eb8799c4d
Hsouna <ala.hassouna.tn@gmail.com>
2024-10-13 16:41:25 +0100

    modification icon onglet

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
+import Head from 'next/head';
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
+        <Head>
+        <link rel="icon" href="/logo.png" />
+        <title>Mon site</title>
+      </Head>
         <body>
           {children}
         </body>
